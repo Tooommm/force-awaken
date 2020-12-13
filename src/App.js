a48495652aa8cba63ca126aecf123da1ced38eca
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-12-14 00:03:18 +0100

    en cours

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ad from './assets/ad.jpg';
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
 const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`;
-const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
+const trans2 = (x, y) => `translate3d(${x / 8 + 1.5}px,${y / 8 - 3.5}px,0)`;
 const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
 const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
 
@@ -22,8 +22,8 @@ function App() {
             <div className="item"> LES EWOKS </div>
             <div className="item"> JABBA LE HUTT </div>
           </div>
-          <animated.div className="card1" style={{ transform: props.xy.interpolate(trans1) }} />
-          <animated.div className="card2" style={{ transform: props.xy.interpolate(trans2) }} />
+          <div class="card3" />
+          <animated.div class="card4" style={{ transform: props.xy.interpolate(trans2) }} />
         </div>
 
         <div className="content">
